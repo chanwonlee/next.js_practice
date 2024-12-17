54351e4452fe1282ff406059153707672f434281
chanwon <clw8679@naver.com>
2024-12-17 18:10:27 +0900

    feat: 문자 기능 구현

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,8 +1,18 @@
+import styles from './message.module.css';
+import Room from "@/app/(afterLogin)/messages/_component/Room";
 
 export default function Home() {
   return (
-    <main>
-      쪽지페이지
+    <main className={styles.main}>
+      <div className={styles.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
     </main>
   )
 }
